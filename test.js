6ee92a43ec357be9a15f785e200e8e7d05826cf6
omch-29 <chandwaniom29@gmail.com>
2025-12-25 15:23:22 +0530

    frontend built

---
+++ test.js
@@ -0,0 +1,14 @@
+const trian = require('./src/services/trian');
+
+const cppCode = `
+void test(int n){
+  for(int i=0;i<n;i++){
+    int x = n;
+    while(x > 1){
+      x /= 2;
+    }
+  }
+}
+`;
+
+console.log(trian.analyzeCPP(cppCode));
