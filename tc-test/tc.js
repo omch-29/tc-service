3c93e73a53c4f2927b7a8c03f2c79055a50050f7
omch-29 <chandwaniom29@gmail.com>
2026-03-29 10:17:11 +0530

    added analyze code

--- tc-test/tc.js
+++ tc-test/tc.js
@@ -2,3 +2,17 @@ import express from express;
 import { analyzeCode } from "../src/controllers/analyze.controller";
 
 
+export const getCode = async(req,res) =>{
+
+}
+export async function get2(req,res){
+    return res.json().status(200); //OK
+    return res.json().status(400) //Bad Request
+    return res.json().status(401) //unauthorized
+    return res.json().status(500) //inernal server error
+}
+ 
+async function get3(params) {
+    
+}
+function get3 = async(request)
